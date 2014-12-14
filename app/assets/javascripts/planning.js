6f9af155a91a0db1fbdde1187b0ea81daba3a0c2
Jean-Baptiste Goulain <jibai31@gmail.com>
2014-12-14 15:20:46 +0000

    Improve design of planning and weeks.

--- app/assets/javascripts/planning.js
+++
@@ -1,6 +0,0 @@
-$(function($){
-  $('.js-preview').click(function(){
-    $(this).hide();
-    $(this).parent().find('.js-plan').css("display", "block");
-  });
-});
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -15,10 +15,10 @@ class Ability
     can [:create, :fill], BufferedPost
 
     # READ
-    can :read, [Blog, Content, BufferedPost], user_id: user.id
+    can :read, [Blog, Content, Account, BufferedPost], user_id: user.id
     can :manage, Message, content: {user_id: user.id}
     can :read, Planning, account: {user_id: user.id}
-    can :read, Account, user: {admin: true}
+    cannot :index, Account, user: {admin: false}
 
     # UPDATE
     can :update, Content, user_id: user.id
