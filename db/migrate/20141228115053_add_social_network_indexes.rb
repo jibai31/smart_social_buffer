6da79c9f947cd746410a2369cdeea4e21805ecd2
Jean-Baptiste Goulain <jibai31@gmail.com>
2015-01-29 10:11:57 +0000

    Cannot remove an index that doesn't exist.

--- db/migrate/20141228115053_add_social_network_indexes.rb
+++ db/migrate/20141228115053_add_social_network_indexes.rb
@@ -1,6 +1,5 @@
 class AddSocialNetworkIndexes < ActiveRecord::Migration
   def change
-    remove_index :accounts, "old_provider_and_uid"
     add_index :accounts, :social_network_id
     add_index :messages, :social_network_id
   end
